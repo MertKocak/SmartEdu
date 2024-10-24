49befdb197485b83d870b8b7ca274d038e34075a
Mert Koçak <mertkocak.2811@gmail.com>
2024-10-24 15:12:39 +0300

    ilk düzenlemeler yapıldı

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require("express");
 
 const app = express();
 
+//template engine
+app.set("view engine", "ejs");
+
+//middlewares
+app.use(express.static("public"))
+
 app.get("/", (req, res) => {
-  res.send(console.log("port 123"));
+  res.status(200).render("index" , {
+    page_name : "index"
+  });
 });
+
+app.get("/about", (req, res) => {
+    res.status(200).render("about" , {
+        page_name : "about"
+      });
+  });
 
 const port = 3000;
 
